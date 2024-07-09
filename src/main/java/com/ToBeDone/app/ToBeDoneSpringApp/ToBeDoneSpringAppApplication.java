f319a697e1dca9b26eddb7383f6eee5456cc5dda
Rich Morban <richardmorban21@gmail.com>
2024-07-08 22:11:29 -0400

    Initial Commit

---
+++ src/main/java/com/ToBeDone/app/ToBeDoneSpringApp/ToBeDoneSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.ToBeDone.app.ToBeDoneSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToBeDoneSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToBeDoneSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ToBeDone/app/ToBeDoneSpringApp/ToBeDoneSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ToBeDone.app.ToBeDoneSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToBeDoneSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
